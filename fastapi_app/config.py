4eceaa7f91075618b4fa9a81dbbb7db5ba7fcbd5
GregTuz <tuzhbag@me.com>
2026-04-11 19:44:22 +0300

    Changed FastApi config + functions

--- fastapi_app/config.py
+++ fastapi_app/config.py
@@ -4,7 +4,9 @@ from pydantic_settings import BaseSettings
 class Settings(BaseSettings):
     csv_path: str = "/app/data/Financial_Transactions_Enriched.csv"
     kafka_bootstrap_servers: str = "kafka:9092"
-    kafka_topic: str = "transactions"
+    kafka_topic_valid: str = "transactions.valid"
+    kafka_topic_invalid: str = "transactions.invalid"
+    broken_transactions_count: int = 500
 
     class Config:
         env_file = ".env"
