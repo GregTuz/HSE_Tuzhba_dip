361bbe1a1238795fc16ac262735ad969e4e36f56
GregTuz <tuzhbag@me.com>
2026-04-12 19:45:41 +0300

    Removed catchup

--- dags/load_currency_rates.py
+++ dags/load_currency_rates.py
@@ -24,7 +24,7 @@ def get_clickhouse_client() -> Client:
 	dag_id="load_currency_rates",
 	schedule="5 9 * * *",
 	start_date=datetime(2015, 12, 31),
-	catchup=True,
+	catchup=False,
 	max_active_runs=1,
 	params={
 		"logical_date": Param(
